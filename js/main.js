cee286637344ac6c5c814292612d99dc237cfbd7
oranthony <anthonyloroscio@gmail.com>
2016-07-13 16:08:43 +0200

    Quality page added.

--- js/main.js
+++ js/main.js
@@ -4,3 +4,14 @@
 $(document).ready(function() {
     $('#simple-menu').sidr();
 });
+
+$(document).ready(function() {
+    $('.collapse.in').prev('.panel-heading').addClass('active');
+    $('#accordion, #bs-collapse')
+        .on('show.bs.collapse', function(a) {
+            $(a.target).prev('.panel-heading').addClass('active');
+        })
+        .on('hide.bs.collapse', function(a) {
+            $(a.target).prev('.panel-heading').removeClass('active');
+        });
+});
